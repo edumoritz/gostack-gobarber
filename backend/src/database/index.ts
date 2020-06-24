719a35f45632a98f68f6592c141ad2412fdf5c04
edumoritz <eduardo.moritz@hotmail.com>
2020-06-24 10:56:34 -0300

    init backend

---
+++ backend/src/database/index.ts
@@ -0,0 +1,4 @@
+import { createConnection } from 'typeorm';
+
+// Executa a connexão do arquivo ormconfig.json, configurado em docker
+createConnection();
---
+++ backend/src/services/CreateAppointmentService.ts
@@ -0,0 +1,39 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '../models/Appointments';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment is already booked');
+    }
+
+    // create: cria um objeto/instancia, não salva no banco de dados.
+    const appointment = appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    // save: salva o objeto no banco de dados
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
