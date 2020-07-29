5635376b74314f4276a631a9c87ab8e29828f250
edumoritz <eduardo.moritz@hotmail.com>
2020-07-29 18:40:48 -0300

    ajustes para testar mobile

--- backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,19 +1,20 @@
 import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
 
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
 export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
-  
+
     const listProviders = container.resolve(ListProvidersService);
-  
+
     const providers = await listProviders.execute({
       user_id,
     });
-  
-    return response.json(providers);
+
+    return response.json(classToClass(providers));
   }
 }
