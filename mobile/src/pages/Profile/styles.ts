6ce27d9bd8f63897027f89ee30304cf324635189
edumoritz <eduardo.moritz@hotmail.com>
2020-07-30 22:10:36 -0300

    finalizando mobile

--- mobile/src/pages/Profile/styles.ts
+++ mobile/src/pages/Profile/styles.ts
@@ -1,36 +1,31 @@
 import styled from 'styled-components/native';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+  position: relative;
 `;
 
-export const Header = styled.View`
-  padding: 24px;
-  padding-top: ${getStatusBarHeight() + 24}px;
-  background: #28262e;
-
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const HeaderTitle = styled.Text`
-  color: #f4ede8;
+export const Title = styled.Text`
   font-size: 20px;
-  font-family: 'RobotoSlab-Regular';
-  line-height: 28px;
-`;
-
-export const UserName = styled.Text`
-  color: #ff9000;
+  color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
+  margin: 24px 0 24px;
 `;
 
-export const ProfileButton = styled.TouchableOpacity``;
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 40px;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity`
+  margin-top: 32px;
+`;
 
 export const UserAvatar = styled.Image`
-  width: 56px;
-  height: 56px;
-  border-radius: 28px;
+  width: 186px;
+  height: 186px;
+  border-radius: 98px;
+  align-self: center;
 `;
